a9111f8f30769fd4adb49e578e482266d9179119
Wuvin <wuvin@outlook.com>
2018-04-11 13:24:56 +0800

    down codeforces's contest data

--- datas/reptile.py
+++ datas/reptile.py
@@ -1,5 +1,7 @@
 import requests
 import bs4
+import scipy.io as scio
+import numpy as np
 
 # problemid(int) solvetime(int seconds used to solve this)
 def hardness(problemid,score,solvetime):
@@ -12,11 +14,9 @@ def getTab(url):
     r = requests.get(url)
     soup = bs4.BeautifulSoup(r.text,'lxml')
     datas = []
-    pids = []
-    pval = []
+    ptitle = []
     for ths in  soup.tr("th")[4:]:
-        pids += ths.a["href"]
-        pval += [int(ths.span.get_text(strip=True))]
+        ptitle += [ths.a["title"][4:]]
     
     for tr in soup(participantid = True):
         tds = tr("td")
@@ -36,7 +36,7 @@ def getTab(url):
     
     for j, line in enumerate(datas):
         temp = []
-        for i in range(3,3+len(pval)):
+        for i in range(3,3+len(ptitle)):
             if line[i] != -1:
                 temp += [[i,line[i]]]
         temp = sorted(temp, key=lambda x:x[1] )
@@ -46,19 +46,38 @@ def getTab(url):
             line[item[0]]=item[1]
         datas[j] = line
 
-    return datas
+    return datas,ptitle
 
 # contest id
 def getAll(contest_id):
+    print("starting to get Contest "+str(contest_id))
     url = "http://codeforces.com/contest/"+str(contest_id)+"/standings"
     r = requests.get(url)
     soup = bs4.BeautifulSoup(r.text,'lxml')
     lens = len(soup("nobr"))
-    ret = getTab(url)
+    ret,title = getTab(url)
     for i in range(2,lens+1):
-        ret += getTab(url + "/page/" + str(i))
-    return ret
+        print("%"+str((i-1)/lens*100)+" finished")
+        nret,_ = getTab(url + "/page/" + str(i))
+        ret += nret
+    print("all finished")
+    return ret,title
 
-data = getAll(956)
-for i in data:
-    print(i)
+def saveRound(contest_id):
+    data,title = getAll(contest_id)
+    data = np.array(data)
+    data = np.asmatrix(data)
+    dataFile = path+str(contest_id)+'.mat'
+    scio.savemat(dataFile, {str(contest_id):data})
+    with open(path+"names.txt","a") as f:
+        print(",".join([str(contest_id)] + title),file = f )
+
+
+path = "C:/Users/wkl31415926/OneDrive/Projects/CPRS/datas/"
+with open(path+"rounds.txt","r") as f:
+    while True:
+        line = f.readline()
+        if line == "":
+            break
+        saveRound(int(line))
+    
